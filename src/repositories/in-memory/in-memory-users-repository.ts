13924565c0269dcbc0718f8dd56600d18269d5ef
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-10-08 15:31:33 -0300

    test: implement RegisterUseCase unit tests

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,27 @@
+import { User, Prisma } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = [];
+
+  async findByEmail(email: string) {
+    const user = this.users.find((u) => u.email === email);
+    if (!user) return null;
+
+    return user;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user: User = {
+      id: "user-1",
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+}
