0228c94d30d17de51beeeadac452941c24126b04
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-10-11 09:40:58 -0300

    feat: implements RBAC (Role Based Authorization Control)

--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -3,15 +3,17 @@ import { FastifyRequest, FastifyReply } from "fastify";
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify({ onlyCookie: true });
 
+  const { role } = request.user;
+
   const token = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: { sub: request.user.sub },
     }
   );
 
   const refreshToken = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
